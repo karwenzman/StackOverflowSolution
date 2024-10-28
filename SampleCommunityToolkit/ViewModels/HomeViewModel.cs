6a94d4c36e055064e98e37c6ecf6617efee1352a
karwenzman <jenning.thorsten@gmx.net>
2024-10-28 17:16:10 +0100

    SampleCommunityToolkit
    Adds a simple to show a strange behavior of automated code is not created.

---
+++ SampleCommunityToolkit/ViewModels/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCommunityToolkit.ViewModels;
+
+public partial class HomeViewModel : ObservableObject
+{
+	[ObservableProperty]
+	private string? _title;
+
+	[ObservableProperty]
+	private string? _copyright;
+	public HomeViewModel()
+    {
+        Title = "Hello World App";
+		
+    }
+}
